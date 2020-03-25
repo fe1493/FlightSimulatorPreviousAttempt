1f1979219f4f158f065a14ed8104db89a89a316d
yonatan gat <yonatan.gat@live.biu.ac.il>
2020-03-25 12:33:08 +0200

    Simulator Model Version1

--- FlightSimulatorApp/Model/ISimulatorModel.cs
+++ FlightSimulatorApp/Model/ISimulatorModel.cs
@@ -17,14 +17,14 @@ namespace FlightSimulatorApp.Model
         string Elevator { set; get; }
         string Aileron { set; get; }
         string Throttle { set; get; }
-        string indicated_heading_deg { set; get; }
-        string gps_indicated_vertical_speed { set; get; }
-        string gps_indicated_ground_speed_kt { set; get; }
-        string airspeed_indicator_indicated_speed_kt { set; get; }
-        string gps_indicated_altitude_ft { set; get; }
-        string attitude_indicator_internal_roll_deg { set; get; }
-        string attitude_indicator_internal_pitch_deg { set; get; }
-        string altimeter_indicated_altitude_ft { set; get; }
+        string Indicated_heading_deg { set; get; }
+        string Gps_indicated_vertical_speed { set; get; }
+        string Gps_indicated_ground_speed_kt { set; get; }
+        string Airspeed_indicator_indicated_speed_kt { set; get; }
+        string Gps_indicated_altitude_ft { set; get; }
+        string Attitude_indicator_internal_roll_deg { set; get; }
+        string Attitude_indicator_internal_pitch_deg { set; get; }
+        string Altimeter_indicated_altitude_ft { set; get; }
         //  "/controls/flight/elevator"
         //  "/controls/flight/aileron"
         //  "/controls/engines/engine/throttle"
---
+++ FlightSimulatorApp/Model/MySimulatorModel.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp.Model
+{
+    class MySimulatorModel : ISimulatorModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void connect(string ip, int port)
+        {
+
+        }
+        public void disconnect()
+        {
+
+        }
+        public void start()
+        {
+
+        }
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        // the property implementation
+        // all the variables from the plane
+        private string rudder;
+        private string elevator;
+        private string aileron;
+        private string throttle;
+        private string indicated_heading_deg;
+        private string gps_indicated_vertical_speed;
+        private string gps_indicated_ground_speed_kt;
+        private string airspeed_indicator_indicated_speed_kt;
+        private string gps_indicated_altitude_ft;
+        private string attitude_indicator_internal_roll_deg;
+        private string attitude_indicator_internal_pitch_deg;
+        private string altimeter_indicated_altitude_ft;
+        public string Rudder {
+            get { return rudder; }
+            set
+            {
+                rudder = value;
+                NotifyPropertyChanged("Rudder");
+            }
+        }
+        public string Elevator {
+            get { return elevator; }
+            set
+            {
+                elevator = value;
+                NotifyPropertyChanged("Elevator");
+            }
+        }
+        public string Aileron {
+            get { return aileron; }
+            set
+            {
+                aileron = value;
+                NotifyPropertyChanged("Aileron");
+            }
+        }
+        public string Throttle {
+            get { return throttle; }
+            set
+            {
+                throttle = value;
+                NotifyPropertyChanged("Throttle");
+            }
+        }
+        public string Indicated_heading_deg {
+            get { return indicated_heading_deg; }
+            set
+            {
+                indicated_heading_deg = value;
+                NotifyPropertyChanged("Indicated_heading_deg");
+            }
+        }
+        public string Gps_indicated_vertical_speed {
+            get { return gps_indicated_vertical_speed; }
+            set
+            {
+                gps_indicated_vertical_speed = value;
+                NotifyPropertyChanged("Gps_indicated_vertical_speed");
+            }
+        }
+        public string Gps_indicated_ground_speed_kt {
+            get { return gps_indicated_ground_speed_kt; }
+            set
+            {
+                gps_indicated_ground_speed_kt = value;
+                NotifyPropertyChanged("Gps_indicated_ground_speed_kt");
+            }
+        }
+        public string Airspeed_indicator_indicated_speed_kt {
+            get { return airspeed_indicator_indicated_speed_kt; }
+            set
+            {
+                airspeed_indicator_indicated_speed_kt = value;
+                NotifyPropertyChanged("Airspeed_indicator_indicated_speed_kt");
+            }
+        }
+        public string Gps_indicated_altitude_ft {
+            get { return gps_indicated_altitude_ft; }
+            set
+            {
+                gps_indicated_altitude_ft = value;
+                NotifyPropertyChanged("Gps_indicated_altitude_ft");
+            }
+        }
+        public string Attitude_indicator_internal_roll_deg {
+            get { return attitude_indicator_internal_roll_deg; }
+            set
+            {
+                attitude_indicator_internal_roll_deg = value;
+                NotifyPropertyChanged("Attitude_indicator_internal_roll_deg");
+            }
+        }
+        public string Attitude_indicator_internal_pitch_deg {
+            get { return attitude_indicator_internal_pitch_deg; }
+            set
+            {
+                attitude_indicator_internal_pitch_deg = value;
+                NotifyPropertyChanged("Attitude_indicator_internal_pitch_deg");
+            }
+        }
+        public string Altimeter_indicated_altitude_ft {
+            get { return altimeter_indicated_altitude_ft; }
+            set
+            {
+                altimeter_indicated_altitude_ft = value;
+                NotifyPropertyChanged("Altimeter_indicated_altitude_ft");
+            }
+        }
+    }
+}
